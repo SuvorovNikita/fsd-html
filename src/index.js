bdcf3ccc664a7c28b04a68ea466d2c165901dbd6
SuvorovNikita <nikit.suvorv@gmail.com>
2020-02-17 17:05:35 +0300

    update webpack-template

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './js/common'
 import './assets/css/main.css'
 import './assets/scss/main.scss'
+function importAll(resolve) {
+    resolve.keys().forEach(resolve);
+  }
+  
+  importAll(require.context('../src/', true, /\.js$|\.scss$/));
